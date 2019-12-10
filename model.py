4ab1b1baef309cc065d2884aba53f44dafe9e59d
ivan <petrovskiim@tut.by>
2019-12-10 14:26:43 +0300

    Weight load from file added!

--- model.py
+++ model.py
@@ -12,7 +12,7 @@ bottleneck = {
     "kernel_size": 1,
     "stride": 1,
     "padding": 0,
-    "bias": True
+    "bias": False
 }
 
 down_sample = {
@@ -28,7 +28,12 @@ casual = {
     "padding": 1,
     "bias": False
 }
-
+prelude = {
+    "kernel_size": 1,
+    "stride": 1,
+    "padding": 0,
+    "bias": True
+}
 
 class Darknet(nn.Module):
     def __init__(self):
@@ -62,7 +67,7 @@ class Darknet(nn.Module):
 
 
 class Tail(nn.Module):
-    def __init__(self):
+    def __init__(self, number_of_classes, anchors_dims):
         super(Tail, self).__init__()
         self.num_of_yolo_layers = 3
         route_streams = [0, 2 ** 3, 2 ** 2]
@@ -92,8 +97,8 @@ class Tail(nn.Module):
                     BatchNorm2d(2 ** (5 - i) * filters_multiplier),
                     LeakyReLU(negative_slope))]) for i in range(self.num_of_yolo_layers)])
         self.preludes = ModuleList([
-            Sequential(Conv2d(2 ** (5 - i) * filters_multiplier, 255, **bottleneck),
-                       BatchNorm2d(255)) for i in range(self.num_of_yolo_layers)])
+            Conv2d(2 ** (5 - i) * filters_multiplier, anchors_dims[i] * (number_of_classes + 5), **prelude)
+            for i in range(self.num_of_yolo_layers)])
         self.equalizers_for_routes = ModuleList([
             Sequential(
                 Conv2d(2 ** (4 - i) * filters_multiplier, 2 ** (3 - i) * filters_multiplier, **bottleneck),
@@ -116,8 +121,17 @@ class Tail(nn.Module):
 
 
 class Head(nn.Module):
-    def __init__(self, anchors, number_classes, network_width):
-         super(YOLO, self).__init__()
+    def __init__(self, anchors, number_of_classes=1, network_shape=[416, 416]):
+        super(Head, self).__init__()
+        self.network_shape = network_shape
+        self.number_of_classes = number_of_classes
+        self.anchors = anchors
+
+    def forward(self, features):
+        grid_size = features.size()[-2:]
+        stride = self.network_shape[0] / grid_size[0]
+
+        grid = 3
 #
 #     def forward(self, tensor):
 #         # batch_size x S x S x (B*5 +C) = 255
@@ -149,8 +163,69 @@ class Head(nn.Module):
 #         class_predictions = sigmoid(prediction_raw[..., 5:]).view(num_batch, -1, number_of_classes)  # Cls pred one-hot.
 #
 #         return cat((bbox_pred, conf_pred, class_predictions), -1)
+#         if targets is None:
+#             return output, 0
+#         else:
+#             iou_scores, \
+#             class_mask, \
+#             obj_mask, \
+#             noobj_mask, \
+#             tx, \
+#             ty, \
+#             tw, \
+#             th, \
+#             tcls, \
+#             tconf = build_targets(
+#                 pred_boxes=pred_boxes,
+#                 pred_cls=pred_cls,
+#                 target=targets,
+#                 anchors=self.scaled_anchors,
+#                 ignore_thres=self.ignore_thres,
+#             )
 #
+#             # Loss : Mask outputs to ignore non-existing objects (except with conf. loss)
+#             loss_x = self.mse_loss(x[obj_mask], tx[obj_mask])
+#             loss_y = self.mse_loss(y[obj_mask], ty[obj_mask])
+#             loss_w = self.mse_loss(w[obj_mask], tw[obj_mask])
+#             loss_h = self.mse_loss(h[obj_mask], th[obj_mask])
+#             loss_conf_obj = self.bce_loss(pred_conf[obj_mask], tconf[obj_mask])
+#             loss_conf_noobj = self.bce_loss(pred_conf[noobj_mask], tconf[noobj_mask])
+#             loss_conf = self.obj_scale * loss_conf_obj + self.noobj_scale * loss_conf_noobj
+#             loss_cls = self.bce_loss(pred_cls[obj_mask], tcls[obj_mask])
+#             total_loss = loss_x + loss_y + loss_w + loss_h + loss_conf + loss_cls
 #
+#             # Metrics
+#             cls_acc = 100 * class_mask[obj_mask].mean()
+#             conf_obj = pred_conf[obj_mask].mean()
+#             conf_noobj = pred_conf[noobj_mask].mean()
+#             conf50 = (pred_conf > 0.5).float()
+#             iou50 = (iou_scores > 0.5).float()
+#             iou75 = (iou_scores > 0.75).float()
+#             detected_mask = conf50 * class_mask * tconf
+#             precision = torch.sum(iou50 * detected_mask) / (conf50.sum() + 1e-16)
+#             recall50 = torch.sum(iou50 * detected_mask) / (obj_mask.sum() + 1e-16)
+#             recall75 = torch.sum(iou75 * detected_mask) / (obj_mask.sum() + 1e-16)
+#
+#             self.metrics = {
+#                 "loss": to_cpu(total_loss).item(),
+#                 "x": to_cpu(loss_x).item(),
+#                 "y": to_cpu(loss_y).item(),
+#                 "w": to_cpu(loss_w).item(),
+#                 "h": to_cpu(loss_h).item(),
+#                 "conf": to_cpu(loss_conf).item(),
+#                 "cls": to_cpu(loss_cls).item(),
+#                 "cls_acc": to_cpu(cls_acc).item(),
+#                 "recall50": to_cpu(recall50).item(),
+#                 "recall75": to_cpu(recall75).item(),
+#                 "precision": to_cpu(precision).item(),
+#                 "conf_obj": to_cpu(conf_obj).item(),
+#                 "conf_noobj": to_cpu(conf_noobj).item(),
+#                 "grid_size": grid_size,
+#             }
+#
+#             return output, total_loss
+
+
 # class YOLO(nn.Module):
 #
 #     def __init__(self, nms=0, thresh=0, hier_thresh=0, post=True):
---
+++ weight_formater.py
@@ -0,0 +1,52 @@
+import numpy as np
+import torch
+import torch
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision import transforms
+from torch.autograd import Variable
+import torch.optim as optim
+
+from model import Darknet, Tail
+
+weight_file_path = "/home/ivan/sets/three_days/backup/v3single_1256008.weights"
+# Open the weights file
+with open(weight_file_path, "rb") as f:
+    header = np.fromfile(f, dtype=np.int32, count=5)  # First five are header values
+    header_info = header  # Needed to write header when saving weights
+    seen = header[3]  # number of images seen during training
+    weights = np.fromfile(f, dtype=np.float32)  # The rest are weights
+print(seen)
+
+image = torch.randn([3, 416, 416])
+darknet = Darknet()
+tail = Tail(1, [3, 3, 3])
+initial_detections = tail(darknet(image.unsqueeze(0)))
+
+
+def parse_darknet_weight(flatten_weights, model):
+    checkpoint = model.state_dict()
+    new_dict = checkpoint.copy()
+    for param_tensor in checkpoint:
+        if param_tensor.endswith("weight") or \
+                param_tensor.endswith("running_mean") or \
+                param_tensor.endswith("running_var") or \
+                param_tensor.endswith("bias"):
+            size_expected_by_model = checkpoint[param_tensor].size()
+
+            length_expected_by_model = np.prod(size_expected_by_model)
+            new_dict[param_tensor] = \
+                torch.from_numpy(flatten_weights[:length_expected_by_model]).view(size_expected_by_model)
+            flatten_weights = flatten_weights[length_expected_by_model:]
+    return new_dict, flatten_weights
+
+
+darknet_state_dict, unused_weights = parse_darknet_weight(weights, darknet)
+tail_state_dict, unused_weights = parse_darknet_weight(unused_weights, tail)
+
+darknet.load_state_dict(darknet_state_dict)
+tail.load_state_dict(tail_state_dict)
+
+print(unused_weights.shape)
+#print(initial_detections[0] == tail(darknet(image.unsqueeze(0)))[0])
+
